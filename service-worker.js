ddd22dac59aa17e4420ee0557326ade2953e7d6b
godzilladhanu <dhanushshetty201@gmail.com>
2021-06-13 16:22:15 +0530

    converted To Web Installable app

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+  self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+    // Add a call to skipWaiting here
+  });
+  
+  self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+  });
+
+  // I'm a new service worker
+  self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+  });
